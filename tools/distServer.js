d5f5dd18d04d6a56bbd911e3a94087ab0a5a2acd
Innocent <innocent.amadi@andela.com>
2016-07-24 00:13:31 +0100

    modify run command in procfile

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import webpack from 'webpack';
 import path from 'path';
-import config from '../webpack.config.dev';
 import open from 'open';
 import compression from 'compression';
 
