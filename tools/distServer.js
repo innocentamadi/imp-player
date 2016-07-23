032551afee5cf50cd7fd24a9dc457fdab223ca68
Innocent <innocent.amadi@andela.com>
2016-07-24 00:21:27 +0100

    modify run command in procfile

--- tools/distServer.js
+++ tools/distServer.js
@@ -5,7 +5,7 @@ import compression from 'compression';
 
 /* eslint-disable no-console */
 
-const port = 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(compression());
